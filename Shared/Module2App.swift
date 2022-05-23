239b0a4fbd833cb286ff3c14109cb70409001088
Kinzie Hamilton <61798938+mackinz2@users.noreply.github.com>
2022-05-23 12:33:17 -0600

    Initial Commit

---
+++ Shared/Module2App.swift
@@ -0,0 +1,17 @@
+//
+//  Module2App.swift
+//  Shared
+//
+//  Created by Kinzie Hamilton on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Module2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
